cbec098d7d263b06564fca2b6c9c94a42f369744
vansh-000 <23165@iiitu.ac.in>
2025-03-02 23:12:25 +0530

    Initialised a Next.js project

---
+++ app/page.js
@@ -0,0 +1,11 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <>
+      <div className="w-screen inline-flex text-4xl h-screen items-center justify-center">
+        Meraki 2K25
+      </div>
+    </>
+  );
+}

6e6c6935fa337692d00410a5961fd44cc8ae7cbe
Pranav <pranav6112004@gmail.com>
2025-03-03 16:32:07 +0530

    asie commit

--- app/page.js
+++ app/page.js
@@ -1,10 +1,12 @@
 import Image from "next/image";
+import TeamsPage from "./_components/Teams_page";
 
 export default function Home() {
   return (
     <>
       <div className="w-screen inline-flex text-4xl h-screen items-center justify-center">
         Meraki 2K25
+        <TeamsPage/>
       </div>
     </>
   );

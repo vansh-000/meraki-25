416664e937a62aeefa39e8aa8f8b3afd79c578f5
vansh-000 <23165@iiitu.ac.in>
2025-03-07 23:07:00 +0530

    refactor: updated ContactForm

--- app/contact/page.jsx
+++ app/contact/page.jsx
@@ -3,10 +3,10 @@ import { ContactForm } from "@/components/ContactForm";
 
 export default function Contact() {
   return (
-    <div className="flex h-screen md:bg-[url('/images/backgrounds/contact.jpeg')] bg-[url('/images/backgrounds/contact.jpg')] bg-cover bg-top md:bg-center w-screen">
+    <div className="flex h-screen md:bg-[url('/images/backgrounds/contact.jpeg')] bg-[url('/images/backgrounds/contact.jpg')] bg-cover bg-top md:bg-center">
       <div className="relative ">
         <Card />
-        <div className="absolute z-50 xl:w-[700px] lg:w-[650px] md:w-[600px] sm:w-[550px] w-[400px] top-15 left-20 ">
+        <div className="absolute w-[50vw] z-50">
           <ContactForm />
         </div>
       </div>

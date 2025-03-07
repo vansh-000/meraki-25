9e708f2daf9bb056d18c4d1fff3bced22ba8a032
vansh-000 <23165@iiitu.ac.in>
2025-03-07 13:48:43 +0530

    fixed footer

--- app/layout.js
+++ app/layout.js
@@ -1,5 +1,6 @@
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import Footer from "@/components/Footer/page";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -24,6 +25,7 @@ export default function RootLayout({ children }) {
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
         {children}
+        <Footer/>
       </body>
     </html>
   );

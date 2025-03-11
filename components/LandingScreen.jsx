39a58109d14a5ce30346d40ce1cb3ebbd6fa7d16
vansh-000 <23165@iiitu.ac.in>
2025-03-12 01:06:57 +0530

    refactor: disable zoom from the cylinder

--- components/LandingScreen.jsx
+++ components/LandingScreen.jsx
@@ -10,26 +10,25 @@ const BackgroundMesh = () => {
     const cyl = useRef(null);
     useFrame((state, delta) => {
         cyl.current.rotation.y += delta;
-    })
+    });
 
     return (
         <group rotation={[0, 1.4, 0.5]}>
-            <mesh ref={cyl} >
+            <mesh ref={cyl}>
                 <cylinderGeometry args={[2, 2, 2, 60, 60, true]} />
                 <meshStandardMaterial map={tex} transparent side={THREE.DoubleSide} />
             </mesh>
         </group>
-
     );
 };
 
 const LandingScreen = () => {
     return (
         <Canvas camera={{ fov: 60 }}>
-            <OrbitControls />
+            <OrbitControls enableZoom={false} />
             <ambientLight />
             <BackgroundMesh />
-        </Canvas >
+        </Canvas>
     );
 };
 

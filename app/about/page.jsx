4c6ae5ccdc48fa9aa77001e1add3ad9152bcfd1f
vansh-000 <23165@iiitu.ac.in>
2025-03-12 00:04:56 +0530

    refactor: Added layout wrapper and restructure layout components for improved organization

--- app/about/page.jsx
+++ app/about/page.jsx
@@ -5,10 +5,10 @@ import { Iceland } from "next/font/google";
 const iceland = Iceland({ subsets: ["latin"], weight: "400" });
 export default function About() {
   return (
-    <div className="h-screen bg-[url('/images/backgrounds/about.jpeg')] md:bg-[url('/images/backgrounds/about1.jpeg')] bg-cover bg-center flex ">
+    <div className=" bg-[url('/images/backgrounds/about.jpeg')] md:bg-[url('/images/backgrounds/about1.jpeg')] bg-cover bg-center flex ">
       <Card />
       <div className="hidden h-screen sm:flex flex-col justify-end items-center md:flex-row  md:justify-start w-full relative ">
-        <div className={`${iceland.className} backdrop-brightness-50 md:backdrop-brightness-100 z-50 w-[90%] h-[41vh] md:w-[42%] md:h-[75%] text-[1rem] sm:text-[1.2rem] md:text-[1.1rem] lg:text-[1.2rem] xl:text-[1.4rem] tracking-wide p-5 md:p-8 mt-10 md:mt-52 lg:mt-68 xl:mt-68 mb-24 text-center md:text-left   `}>
+        <div className={`${iceland.className} backdrop-brightness-50 md:backdrop-brightness-100 z-50 h-[41vh] w-[60%] md:w-[49%] md:h-[75%] text-[1rem] sm:text-[1.2rem] md:text-[1.1rem] lg:text-[1.2rem] xl:text-[1.4rem] tracking-wide p-5 md:p-8 mt-10 md:mt-52 lg:mt-68 xl:mt-68 mb-24 text-center `}>
           MERAKI, the technical fest of IIIT UNA, is a hub of innovation,
           creativity, and technology. It brings together dreamers, creators, and
           tech enthusiasts to explore ideas, collaborate, and push boundaries.
@@ -19,7 +19,7 @@ export default function About() {
           innovation, discovery, and endless possibilities!
         </div>
 
-        <div className="absolute top-1/6  sm:left-[35%] lg:top-[26.2%] lg:left-[43%] md:top-[30%] md:left-[44%] z-50 icon">
+        <div className="absolute top-1/6 lg:top-[26.2%] lg:left-[62%] md:top-[30%] md:left-[50%] z-50 icon">
           <Image
             src={merakiAboutLogo}
             alt="merakiAboutLogo"
@@ -37,15 +37,15 @@ export default function About() {
         </div>
       </div>
 
-      <div className="sm:hidden flex flex-col p-8 gap-10 mt-20 mb-20 items-center">
-        <div className="  z-50 relative">
+      <div className="sm:hidden flex flex-col p-4 gap-10 mt-20 mb-10 items-center">
+        <div className=" z-50 relative">
           <Image
             src={merakiAboutLogo}
             alt="merakiAboutLogo"
-            className="z-100 w-[150px] h-[150px] top-[13%] left-[13%]  absolute  "
+            className="z-100 w-[100px] h-[100px] top-[17%] left-[17%]  absolute  "
           />
           <div
-            className=" w-[200px] h-[200px]  grid grid-cols-2 "
+            className=" w-[150px] h-[150px]  grid grid-cols-2 "
             style={{
               clipPath:
                 "polygon(0% 0%, 0% 100%, 6% 94%, 6% 6%, 94% 6%, 94% 94%, 6% 94%, 0 100%, 100% 100%, 100% 0%)",
@@ -55,16 +55,10 @@ export default function About() {
             <div className="bg-black"></div>
           </div>
         </div>
-        <div className={`${iceland.className} p-8 backdrop-brightness-50 max-h-[38vh] overflow-y-scroll snap-y snap-mandatory scroll-hidden`}>
-          <p className="snap-start py-4"> MERAKI, the technical fest of IIIT UNA, is a hub of innovation,
-          creativity, and technology. It brings together dreamers, creators, and
-          tech enthusiasts to explore ideas, collaborate, and push boundaries.
-          With competitions, workshops, speaker sessions, and exhibitions, MERAKI
-          offers an immersive experience. Whether you're a coder, designer, or
-          engineer, showcase your skills, gain insights, and be part of a
-          community shaping the future. Join us at MERAKI and embrace innovation,
-          discovery, and endless possibilities! </p>
-          
+        <div className={`${iceland.className} p-4 text-center
+         backdrop-brightness-50`}>
+          MERAKI, the technical fest of IIIT UNA, is a hub of innovation, creativity, and technology. It brings together dreamers, creators, and tech enthusiasts to explore ideas, collaborate, and push boundaries. With competitions, workshops, speaker sessions, and exhibitions, MERAKI offers an immersive experience.<br />
+          Whether you're a coder, designer, or engineer, showcase your skills, gain insights, and be part of a community shaping the future. Join us at MERAKI and embrace innovation, discovery, and endless possibilities!
         </div>
       </div>
     </div>
--- app/page.js
+++ app/page.js
@@ -3,39 +3,41 @@ import HomeSponsors from "@/components/HomeSponsors";
 import HomeSlider from "@/components/HomeSlider";
 import HomeContact from "@/components/HomeContact";
 import LandingScreen from "@/components/LandingScreen";
+import Footer from "@/components/Footer/Footer";
 
 export default function Home() {
   return (
-    <div
-      className=" snap-y snap-mandatory overflow-y-auto"
-      style={{
-        scrollbarWidth: "none", // For Firefox
-        msOverflowStyle: "none", // For IE/Edge
-      }}
-    >
-      {/* Hide scrollbar for WebKit browsers */}
-      <style jsx>{`
-        div::-webkit-scrollbar {
-          display: none;
-        }
-      `}</style>
+    <div className="overflow-y-auto">
+      <div
+        className="h-screen overflow-y-scroll snap-y snap-mandatory"
+        style={{
+          scrollbarWidth: "none",
+          msOverflowStyle: "none",
+        }}
+      >
+        <style jsx>{`
+          div::-webkit-scrollbar {
+            display: none;
+          }
+        `}</style>
 
-      <div className="h-screen bg-white snap-start">
-        <LandingScreen />
-      </div>
+        <section className="h-screen snap-start bg-black">
+          <LandingScreen />
+        </section>
 
-      <div className="h-screen snap-start">
-        <HomeSlider />
-      </div>
-      
-      <div className="h-screen snap-start">
-        <HomeSponsors />
-      </div>
+        <section className="h-screen snap-start">
+          <HomeSlider />
+        </section>
 
-      <div className=" h-screen snap-start">
-        <HomeContact />
-      </div>
+        <section className="h-screen snap-start">
+          <HomeSponsors />
+        </section>
 
+        <section className="h-screen snap-start ">
+          <HomeContact />
+          <Footer />
+        </section>
+      </div>
     </div>
   );
 }

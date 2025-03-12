dc923bfa88c3eda26e3d6aead988035060885149
vansh-000 <23165@iiitu.ac.in>
2025-03-12 21:37:40 +0530

    landing page background changed

--- components/LandingScreen.jsx
+++ components/LandingScreen.jsx
@@ -6,16 +6,16 @@ import React, { useRef } from "react";
 import * as THREE from "three";
 
 const BackgroundMesh = () => {
-    const tex = useTexture("/images/image.png");
+    const tex = useTexture("/images/image1.png");
     const cyl = useRef(null);
     useFrame((state, delta) => {
-        cyl.current.rotation.y += delta;
+        cyl.current.rotation.y += delta * 0.8;
     });
 
     return (
-        <group rotation={[0, 1.4, 0.5]}>
+        <group rotation={[0, 1.4, 0.4]}>
             <mesh ref={cyl}>
-                <cylinderGeometry args={[2, 2, 2, 60, 60, true]} />
+                <cylinderGeometry args={[2, 2, 2, 100, 100, true]} />
                 <meshStandardMaterial map={tex} transparent side={THREE.DoubleSide} />
             </mesh>
         </group>
@@ -24,7 +24,7 @@ const BackgroundMesh = () => {
 
 const LandingScreen = () => {
     return (
-        <Canvas camera={{ fov: 60 }}>
+        <Canvas camera={{ fov: 50 }}>
             <OrbitControls enableZoom={false} />
             <ambientLight />
             <BackgroundMesh />

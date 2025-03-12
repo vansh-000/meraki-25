f28c860c2f447e30bda819c03b3a00635882ab27
vansh-000 <23165@iiitu.ac.in>
2025-03-12 21:45:34 +0530

    refactor: adjusted cylinder size based on window width

--- components/LandingScreen.jsx
+++ components/LandingScreen.jsx
@@ -2,20 +2,35 @@
 import { OrbitControls, useTexture } from "@react-three/drei";
 import { Bloom, EffectComposer } from '@react-three/postprocessing'
 import { Canvas, useFrame } from "@react-three/fiber";
-import React, { useRef } from "react";
+import React, { useRef, useState, useEffect } from "react";
 import * as THREE from "three";
 
 const BackgroundMesh = () => {
     const tex = useTexture("/images/image1.png");
     const cyl = useRef(null);
+    const [size, setSize] = useState([2, 2, 2]);
+
+    useEffect(() => {
+        const updateSize = () => {
+            if (window.innerWidth < 768) {
+                setSize([1.2, 1.2, 1.2]);
+            } else {
+                setSize([2, 2, 2]);
+            }
+        };
+        updateSize();
+        window.addEventListener("resize", updateSize);
+        return () => window.removeEventListener("resize", updateSize);
+    }, []);
+
     useFrame((state, delta) => {
         cyl.current.rotation.y += delta * 0.8;
     });
 
     return (
         <group rotation={[0, 1.4, 0.4]}>
-            <mesh ref={cyl}>
-                <cylinderGeometry args={[2, 2, 2, 100, 100, true]} />
+            <mesh ref={cyl} scale={size}>
+                <cylinderGeometry args={[1, 1, 1, 100, 100, true]} />
                 <meshStandardMaterial map={tex} transparent side={THREE.DoubleSide} />
             </mesh>
         </group>

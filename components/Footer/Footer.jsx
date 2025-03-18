43c06c999f5c6ab5bf836609135432900bce0dd8
vansh-000 <23165@iiitu.ac.in>
2025-03-18 22:37:50 +0530

    Refactor: Updated Footer layout

--- components/Footer/Footer.jsx
+++ components/Footer/Footer.jsx
@@ -38,9 +38,10 @@ const Footer = () => {
           </div>
         </div>
       </div>
-      <div className={`md:hidden relative mt-[-40px] bottom-0 ${iceland.className} left-0 right-0 bg-black w-full h-20 flex flex-col justify-center items-center px-6 py-2 z-50`}>
+      <div className={`md:hidden flex relative mt-[-40px] bottom-0 ${iceland.className} left-0 right-0 bg-black w-full h-20 flex flex-row justify-around items-center px-6 py-2 z-50`}>
         <h2 className="text-lg tracking-wide text-white">MERAKI - AY'25</h2>
-        <h2 className="text-lg tracking-wide text-white">IIIT UNA</h2>
+
+
         <div className="flex gap-2 mt-2">
           <a href="#">
             <Image src={insta} alt="Follow us on Instagram" className="w-6 h-6" />
@@ -49,6 +50,7 @@ const Footer = () => {
             <Image src={linkedIn} alt="Connect with us on LinkedIn" className="w-6 h-6" />
           </a>
         </div>
+        <h2 className="text-lg tracking-wide text-white">IIIT UNA</h2>
       </div>
     </>
   );
